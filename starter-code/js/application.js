8466f410df89af014d012ca1fd00563af58be393
Jeff <jzinger100@gmail.com>
2017-01-23 20:49:16 +0100

    trying to make user selected operator work, rest of the code is still stable

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -3,13 +3,40 @@
 //Initialize ion library
 
 
-window.onload = function(){
+// window.onload = function(){
+//
+// };
 
-};
-
-console.log(document.getElementById("start"));
-
-document.getElementById("start").onclick = function(){
-  document.getElementById("game-started").setAttribute("class", "display");
-  document.getElementById("start-screen").setAttribute("class", "display-none");
-};
+// var userOperator;
+//
+// function getUserOperator(){
+//   var numberOfCheckboxes = document.getElementsByClassName("userOperator").length;
+//   var checkboxes = document.getElementsByClassName("userOperator");
+//
+//   for (var i=0; i < numberOfCheckboxes; i++){
+//     if(checkboxes[i].checked == true) {
+//       switch(checkboxes[i].id){
+//         case "addCheck":
+//           userOperator = "+";
+//           break;
+//         case "minusCheck":
+//           userOperator = "-";
+//           break;
+//         case "multCheck":
+//           userOperator = "*";
+//           break;
+//         case "divideCheck":
+//           userOperator = "/";
+//           break;
+//       }
+//     }
+//   }
+//
+//   console.log(userOperator);
+// }
+//
+// document.getElementById("start").onclick = function(){
+//   getUserOperator();
+//   document.getElementById("game-started").setAttribute("class", "display");
+//   document.getElementById("start-screen").setAttribute("class", "display-none");
+// };
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -2,6 +2,8 @@
 
 var userOperation, userTopLimit;
 
+// New game constructor
+
 function TenSecondsMathGame(operation, topLimit) {
     this.userOperation = operation;
     this.userTopLimit = topLimit;
@@ -9,8 +11,51 @@ function TenSecondsMathGame(operation, topLimit) {
     this.isCorrectAnswer = isCorrectAnswer;
 }
 
-var game = new TenSecondsMathGame("-",10);
+//Assign operator according to button checked - when start button is clicked.
 
+var userOperator;
+
+function getUserOperator(){
+  var numberOfCheckboxes = document.getElementsByClassName("userOperator").length;
+  var checkboxes = document.getElementsByClassName("userOperator");
+
+  for (var i=0; i < numberOfCheckboxes; i++){
+    if(checkboxes[i].checked == true) {
+      switch(checkboxes[i].id){
+        case "addCheck":
+          userOperator = "+";
+          break;
+        case "minusCheck":
+          userOperator = "-";
+          break;
+        case "multCheck":
+          userOperator = "*";
+          break;
+        case "divideCheck":
+          userOperator = "/";
+          break;
+      }
+    }
+  }
+
+  console.log(userOperator);
+}
+
+document.getElementById("start").onclick = function(){
+  getUserOperator();
+  document.getElementById("game-started").setAttribute("class", "display");
+  document.getElementById("start-screen").setAttribute("class", "display-none");
+};
+
+console.log(userOperator);
+
+//Assign limit based on what user has selected on sliding scale
+
+
+
+//Generate new game based on user operator and limit selected
+
+var game = new TenSecondsMathGame("-",10);
 
 // Returns a random integer between [1..numberLimit]
 
@@ -24,14 +69,17 @@ var random1 = randomGen();
 var random2 = randomGen();
 var answer;
 
+//Reruns random number generators if difference is between 0 and 3
+
 function questionGen() {
   var reRunRandom = function(){
     while((random1 - random2 < 3 &&  random1 - random2 >= 0) || (random2 - random1 < 3 && random2 - random1 >= 0)){
     random1 = randomGen();
     random2 = randomGen();
     }
-
   };
+
+  //Generates correct answer to question generated
 
   switch(this.userOperation) {
     case '+':
@@ -56,18 +104,29 @@ function questionGen() {
 
   }
 
+
+//HTML section
+
+//Shows which operator has been selected in HTML
+
 document.getElementById("operation").innerHTML = this.userOperation;
 
-if (random1 > random2){
-  document.getElementById("random1").innerHTML = random1;
-  document.getElementById("random2").innerHTML = random2;
-} else {
-  document.getElementById("random1").innerHTML = random2;
-  document.getElementById("random2").innerHTML = random1;
- }
+//Switched HTML numbers depending on which is bigger
+
+  if (random1 > random2){
+    document.getElementById("random1").innerHTML = random1;
+    document.getElementById("random2").innerHTML = random2;
+  } else {
+    document.getElementById("random1").innerHTML = random2;
+    document.getElementById("random2").innerHTML = random1;
+  }
 }
 
+//Function to start first game, HTML not shown until start game button is clicked
 game.newQuestion();
+
+
+//function to run new game
 
 function newGame() {
   random1 = randomGen();
@@ -76,6 +135,8 @@ function newGame() {
   document.getElementById("answer").value = "";
   document.getElementById("answer").style.borderColor = "inherit";
 }
+
+//function to check if correct answer - runs new game function if answer is correct
 
 function isCorrectAnswer(userAnswer) {
   if (userAnswer == answer) {
@@ -90,14 +151,9 @@ function isCorrectAnswer(userAnswer) {
 
 }
 
-
-
-
+//code to check user answer - run when user submit button is pressed
 
 function checkAnswer() {
   var userAnswer = document.getElementById('answer').value;
   isCorrectAnswer(userAnswer);
 }
-
-
-// Checks a user answer
